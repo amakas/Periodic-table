2cd4000340e6a679eca1bee46e71b092d07de24e
Maks Yakoviuk <maksyakovyuk@gmail.com>
2025-05-08 18:29:57 +0000

    feat: change file

--- element.sh
+++ element.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-NUMBERS=$($PSQL "SELECT atomic_number FROM elements")
-SYMBOLS=$($PSQL "SELECT symbol FROM elements")
-NAMES=$($PSQL "SELECT name FROM elements")
+# Перевірка, чи введено значення
 if [[ -z $1 ]]
 then
-echo Please provide an element as an argument.
+  echo Please provide an element as an argument.
 else
-  if echo "$NUMBERS" | grep -qw "$1" || echo "$SYMBOLS" | grep -qw "$1" || echo "$NAMES" | grep -qw "$1" ;
+  # Перевірка, чи введене значення є числом (atomic_number)
+  if [[ $1 =~ ^[0-9]+$ ]]
   then
-  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = '$1' OR symbol = '$1' OR  name = '$1'")
-  NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number = '$1' OR symbol = '$1' OR  name = '$1'")
-  SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number = '$1' OR symbol = '$1' OR  name = '$1'")
-  TYPE=$($PSQL "SELECT type FROM elements JOIN properties ON elements.atomic_number = properties.atomic_number JOIN types ON properties.type_id = types.type_id WHERE atomic_number = '$1' OR symbol = '$1' OR  name = '$1'")
-  MASS=$($PSQL "SELECT atomic_mass FROM elements JOIN properties USING (atomic_number) WHERE atomic_number = '$1' OR symbol = '$1' OR  name = '$1'")
-  MELTING=$($PSQL "SELECT melting_point_celsius FROM elements JOIN properties USING (atomic_number) WHERE atomic_number = '$1' OR symbol = '$1' OR  name = '$1'")
-  BOILING=$($PSQL "SELECT boiling_point_celsius FROM elements JOIN properties USING (atomic_number) WHERE atomic_number = '$1' OR symbol = '$1' OR  name = '$1'")
-  echo "The element with atomic number $ATOMIC_NUMBER is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
-    else 
+    # Якщо це число, шукаємо за atomic_number
+    ELEMENT_INFO=$($PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties ON elements.atomic_number = properties.atomic_number JOIN types ON properties.type_id = types.type_id WHERE elements.atomic_number = '$1'")
+  else
+    # Якщо це не число, шукаємо за символом або назвою
+    ELEMENT_INFO=$($PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties ON elements.atomic_number = properties.atomic_number JOIN types ON properties.type_id = types.type_id WHERE elements.symbol = '$1' OR elements.name = '$1'")
+  fi
+
+  # Перевірка, чи знайдений елемент
+  if [[ -z $ELEMENT_INFO ]]
+  then
     echo I could not find that element in the database.
-    
+  else
+    # Парсинг результатів
+    ATOMIC_NUMBER=$(echo $ELEMENT_INFO | cut -d'|' -f1)
+    NAME=$(echo $ELEMENT_INFO | cut -d'|' -f2)
+    SYMBOL=$(echo $ELEMENT_INFO | cut -d'|' -f3)
+    TYPE=$(echo $ELEMENT_INFO | cut -d'|' -f4)
+    MASS=$(echo $ELEMENT_INFO | cut -d'|' -f5)
+    MELTING=$(echo $ELEMENT_INFO | cut -d'|' -f6)
+    BOILING=$(echo $ELEMENT_INFO | cut -d'|' -f7)
+
+    # Виведення результату
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
   fi
 fi
